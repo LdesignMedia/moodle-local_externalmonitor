677ab15dcc71c6cf51971f48c7b60b6b661cd04a
Luuk Verhoeven <luuk@ldesign.nl>
2023-11-08 10:20:00 +0100

    Add debug setting

--- lib.php
+++ lib.php
@@ -30,6 +30,12 @@
 function local_externalmonitor_after_config() {
     global $PAGE, $CFG;
 
+    $enabled = get_config('local_externalmonitor', 'enable_debugging');
+
+    if (empty($enabled)) {
+        return;
+    }
+
     // TODO setting in admin to enable/disable this feature.
     if ($_SERVER['PHP_SELF'] !== '/webservice/rest/server.php' && $_SERVER['PHP_SELF'] !== '/auth/saml2/sp/saml2-acs.php' ||
         (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] === 'local_externalmonitor')) {
